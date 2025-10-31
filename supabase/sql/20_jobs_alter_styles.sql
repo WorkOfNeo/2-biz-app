f0d8718a4bebdd3d7c852df6faaefd019681adc6
Neo <nh@neo-labs.com>
2025-10-31 17:27:20 +0100

    chore: noop comment to trigger redeploy

--- supabase/sql/20_jobs_alter_styles.sql
+++ supabase/sql/20_jobs_alter_styles.sql
@@ -5,3 +5,5 @@ alter table public.jobs drop constraint if exists jobs_type_check;
 alter table public.jobs add constraint jobs_type_check check (type in ('scrape_statistics','scrape_styles'));
 
 
+
+-- noop change to trigger redeploy
